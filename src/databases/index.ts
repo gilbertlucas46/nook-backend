75b223c8f353b3ed0a3242acf876ddab0199b134
Anurag <anurag.agarwal@appinventiv.com>
2019-08-28 11:59:06 +0530

    credentials updated

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,37 +1,65 @@
 import { connect, set, connection as db } from 'mongoose';
 import * as config from 'config';
 const dbUrl = config.get<string>('dbConfig.dbUrl');
+const dbUserName = config.get<string>('dbConfig.dbUser');
+const dbUserPwd = config.get<string>('dbConfig.dbPwd');
 const displayColors = config.get('displayColors');
 const environment = config.get<string>('environment');
 
 export class Database {
-   async connectDatabase() {
-     console.log('environment=====>>>',environment);
-     
+
+  async connectDatabase() {
     set('debug', true);
-   db.on('error', err => { console.error('%s', err) })
-     .on('close', () => console.log('Database connection closed.'))
+    db.on('error', err => { console.error('%s', err) })
+      .on('close', () => console.log('Database connection closed.'))
+      
     switch (environment) {
       case "default": {
-         console.log('connected to the db');        
-        connect(dbUrl, { useCreateIndex: true, useNewUrlParser: true })
+        connect(dbUrl, {
+          auth: {
+            user: dbUserName,
+            password: dbUserPwd,
+          },
+          useCreateIndex: true,
+          useNewUrlParser: true
+        })
         break;
       }
       case "development": {
-        connect(dbUrl, { useCreateIndex: true, useNewUrlParser: true })
+        connect(dbUrl, {
+          auth: {
+            user: dbUserName,
+            password: dbUserPwd,
+          },
+          useCreateIndex: true,
+          useNewUrlParser: true
+        })
         break
       }
       case "testing": {
-        connect(dbUrl, { useCreateIndex: true, useNewUrlParser: true })
-        break
+        connect(dbUrl, {
+          auth: {
+            user: dbUserName,
+            password: dbUserPwd,
+          },
+          useCreateIndex: true,
+          useNewUrlParser: true
+        })
+        break;
       }
       case "staging": {
-        connect(dbUrl, { w: "majority", journal: true, useCreateIndex: true, useNewUrlParser: true })
-        break
-     }
+        connect(dbUrl, {
+          auth: {
+            user: dbUserName,
+            password: dbUserPwd,
+          },
+          useCreateIndex: true,
+          useNewUrlParser: true
+        })
+        break;
+      }
     }
     console.info(displayColors ? '\x1b[32m%s\x1b[0m' : '%s', `Connected to ${dbUrl}`)
     return {}
   }
-  
 }
