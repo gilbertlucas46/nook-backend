68bbbdb3a0c40a89a03bdfdbcfb77384df27e64d
Shubham S <shubham.maheshwari@appinventiv.com>
2019-09-12 15:59:33 +0530

    swagger stattud

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -13,7 +13,7 @@ export let plugin = {
                 title: 'NOOK_APP API 1.0',
                 version: 'v1',
             },
-            "schemes": ["http","https"],
+            "schemes": ["https"],
             'securityDefinitions': {
                 'api_key': {
                     'type': 'apiKey',
