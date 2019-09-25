3d984ac18549df85e6db79229c685d55fd985d8c
Shubham S <shubham.maheshwari@appinventiv.com>
2019-09-25 12:26:28 +0530

    update article

--- src/entity/article.entity.ts
+++ src/entity/article.entity.ts
@@ -1,14 +1,93 @@
 'use strict';
 import { BaseEntity } from './base.entity';
-import { EnquiryRequest } from '../interfaces/enquiry.interface';
+import { ArticleRequest } from '../interfaces/article.interface';
 import { UserRequest } from '../interfaces/user.interface';
-
+import * as Constant from '../constants';
+import * as utils from '../utils';
+import { Types } from 'mongoose';
 export class ArticleClass extends BaseEntity {
     constructor() {
         super('Article');
     }
 
-    async enquiryList(payload: EnquiryRequest.GetEnquiry, userData: UserRequest.UserData) {
+    async getArticlelist(payload: ArticleRequest.GetArticle) {
+        try {
+            let { page, limit, sortBy, sortType } = payload;
+            if (!limit) { limit = Constant.SERVER.LIMIT; } else { limit = limit; }
+            if (!page) { page = 1; } else { page = page; }
+            let sortingType = {};
+            sortType = !sortType ? -1 : sortType;
+            // const matchObject: any = { $match: {} };
+            let query;
+            sortingType = {
+                createdAt: sortType,
+            };
+            // if (sortBy) {
+            //     switch (sortBy) {
+            //         case 'price':
+            //             sortBy = 'price';
+            //             sortingType = {
+            //                 'property_basic_details.sale_rent_price': sortType,
+            //             };
+            //             break;
+            //         case 'date':
+            //             sortBy = 'date';
+            //             sortingType = {
+            //                 createdAt: sortType,
+            //             };
+            //             break;
+            //         case 'isFeatured':
+            //             sortBy = 'isFeatured';
+            //             sortingType = {
+            //                 isFeatured: sortType,
+            //             };
+            //         default:
+            //             sortBy = 'createdAt';
+            //             sortingType = {
+            //                 createdAt: sortType,
+            //             };
+            //             break;
+            //     }
+            // }
+
+            if (payload.categoryId) {
+                query = {
+                    categoryId: payload.categoryId,
+                    status: Constant.DATABASE.ARTICLE_STATUS.ACTIVE.NUMBER,
+
+                };
+            } else if (payload.articleId) {
+                query = {
+                    _id: Types.ObjectId(payload.articleId),
+                };
+            } else {
+                query = {
+                    status: Constant.DATABASE.ARTICLE_STATUS.ACTIVE.NUMBER,
+                };
+            }
+            const pipeline = [
+                {
+                    $match: query,
+                },
+                { $sort: sortingType },
+            ];
+
+            if (payload.articleId) {
+                const articleList = await this.DAOManager.findOne(this.modelName, query, {});
+                console.log('articleList', articleList);
+                return articleList;
+            } else {
+                const articleList = await this.DAOManager.paginate(this.modelName, pipeline, limit, page);
+                return articleList;
+            }
+
+        } catch (error) {
+            utils.consolelog('errrorArticlelist', error, true);
+            return Promise.reject(error);
+        }
+    }
+
+    async showAllArticle() {
         try {
 
         } catch (error) {
