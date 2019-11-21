c2e8ec017bde9cc8a3067693da8f031d91a14868
rajat.maheshwari <rajat.maheshwari@appinventiv.com>
2019-11-21 19:12:11 +0530

    changes

---
+++ src/models/subscription/subscription.model.ts
@@ -0,0 +1,33 @@
+import { Schema, Document, Types, model } from 'mongoose';
+
+import * as CONSTANT from '../../constants/app.constant';
+
+export interface ISubscription extends Document {
+    featuredType: string;
+    propertyId?: Types.ObjectId;
+    userId?: Types.ObjectId;
+    startDate: number;
+    endDate: number;
+    createdAt: number;
+    updatedAt: number;
+}
+
+const subscriptionSchema = new Schema({
+    _id: { type: Schema.Types.ObjectId, required: true, auto: true },
+    featuredType: {
+        type: String,
+        enum: [
+            CONSTANT.DATABASE.FEATURED_TYPE.PROFILE,
+            CONSTANT.DATABASE.FEATURED_TYPE.PROPERTY,
+            CONSTANT.DATABASE.FEATURED_TYPE.HOMEPAGE
+        ]
+    },
+    propertyId: { type: Schema.Types.ObjectId, ref: 'Property' },
+    userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    endDate: { type: Number },
+    startDate: { type: Number },
+    createdAt: { type: Number, required: true },
+    updatedAt: { type: Number, required: true }
+});
+
+export const Subscription = model<ISubscription>('Subscription', subscriptionSchema);
