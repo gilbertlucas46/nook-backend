246860fcabca9b5f42d4990fca3ea87b29871847
Shubham S <shubham.maheshwari@appinventiv.com>
2019-11-28 00:11:53 +0530

    update

--- src/models/article/article.model.ts
+++ src/models/article/article.model.ts
@@ -45,7 +45,7 @@ const articleSchema = new Schema({
             Constant.DATABASE.ARTICLE_TYPE.HOME_LOANS.TYPE,
             Constant.DATABASE.ARTICLE_TYPE.RENTING.TYPE,
             Constant.DATABASE.ARTICLE_TYPE.SELLING.TYPE,
-            Constant.DATABASE.ARTICLE_TYPE.NEWS.NUMBER,
+            Constant.DATABASE.ARTICLE_TYPE.NEWS.TYPE,
         ], index: true,
     },
     imageUrl: { type: String },
