302e97546bfa7b0e9e9fd7666f1ba6bfa4e07a40
rajat.maheshwari <rajat.maheshwari@appinventiv.com>
2019-11-28 19:59:08 +0530

    changes

--- src/routes/user/agent.routes.ts
+++ src/routes/user/agent.routes.ts
@@ -69,7 +69,8 @@ export let agentRoute: ServerRoute[] = [
                             ),
                         ),
                     searchBy: Joi.string().valid('company', 'name', 'location').default('location'),
-					screenType: Joi.string().trim().required().valid([Constant.DATABASE.SCREEN_TYPE.HOMEPAGE, Constant.DATABASE.SCREEN_TYPE.SEARCH]).default(Constant.DATABASE.SCREEN_TYPE.HOMEPAGE),
+                    // screenType: Joi.string().trim().required().valid([Constant.DATABASE.SCREEN_TYPE.HOMEPAGE, Constant.DATABASE.SCREEN_TYPE.SEARCH]).default(Constant.DATABASE.SCREEN_TYPE.HOMEPAGE), // uncomment later
+                    screenType: Joi.string().trim().optional().valid([Constant.DATABASE.SCREEN_TYPE.HOMEPAGE, Constant.DATABASE.SCREEN_TYPE.SEARCH]).default(Constant.DATABASE.SCREEN_TYPE.HOMEPAGE), // remove later
                 },
                 headers: UniversalFunctions.authorizationHeaderObj,
                 failAction: UniversalFunctions.failActionFunction,
