3c189ad795af2b00eadf974e4c49df102dd2cfa0
Shubham S <shubham.maheshwari@appinventiv.com>
2019-12-13 16:56:02 +0530

    add

--- src/entity/article/article.entity.ts
+++ src/entity/article/article.entity.ts
@@ -267,6 +267,7 @@ export class ArticleClass extends BaseEntity {
             }
             else {
                 query = {
+                    isFeatured: true,
                     status: Constant.DATABASE.ARTICLE_STATUS.ACTIVE,
                 };
             }
