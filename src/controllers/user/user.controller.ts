edb895627ea3dd0da32a508982944fc97be7d4a2
Shubham S <shubham.maheshwari@appinventiv.com>
2019-12-19 15:25:45 +0530

    u[pdate

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -151,6 +151,7 @@ export class UserController {
 						firstName: updateUser.firstName,
 						lastName: updateUser.lastName,
 						userType: updateUser.type,
+						email: getUser.email,
 					},
 					isProfileComplete: true,
 				};
