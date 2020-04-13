66026b7f9773fb098dc2aaf2f8ecc78b3296330f
Shubham S <shubham.maheshwari@appinventiv.com>
2020-04-13 12:30:29 +0530

    update

---
+++ src/entity/preQualification/preQualification.ts
@@ -0,0 +1,24 @@
+abbrevation: "BPI"
+bankFeeAmount: 0
+bankFeePercent: "up to 2%"
+bankId: "5df7801cff3306fb47b485ac"
+bankName: "BPI Family Savings Bank"
+bannerUrl: "/assets/banking/bpi-bank/banner.jpg"
+debtIncomePercentRatio: 0.4426655275896422
+debtIncomeRatio: 40
+fixingPeriod: 1
+grossIncome: 98765433
+headquarterLocation: "Makati"
+iconUrl: "/assets/banking/bpi-bank/icon.jpg"
+interestRate: 5.88
+loanApplicationFeeAmount: 0
+loanApplicationFeePercent: 0
+loanDurationMonthly: 24
+loanDurationYearly: 2
+loanForCancelledCreditCard: true
+loanableAmount: 9876542
+logoUrl: "/assets/banking/bpi-bank/logo.jpg"
+maxLoanDurationAllowed: 20
+monthlyPayment: 437200.5250656446
+processingTime: "As fast as 5 working days upon submission of complete documents"
+totalLoanMonthly: 437200.5250656446
