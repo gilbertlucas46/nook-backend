af510b67decda1c1bb9405b2e68bf8e70f6a53d5
Shubham S <shubham.maheshwari@appinventiv.com>
2020-04-18 02:11:55 +0530

    update

--- src/routes/preQualification/preQualification.routes.ts
+++ src/routes/preQualification/preQualification.routes.ts
@@ -37,136 +37,136 @@ const objectSchema = Joi.object({
 });
 
 export let preQualificationroutes: ServerRoute[] = [
-    {
-        method: 'POST',
-        path: '/v1/user/prequalification',
-        handler: async (request, h) => {
-            try {
-                const userData = request.auth && request.auth.credentials && (request.auth.credentials as any).userData;
-                const payload: PreQualificationRequest.IPreLoanAdd = request.payload as any;
+    // {
+    //     method: 'POST',
+    //     path: '/v1/user/prequalification',
+    //     handler: async (request, h) => {
+    //         try {
+    //             const userData = request.auth && request.auth.credentials && (request.auth.credentials as any).userData;
+    //             const payload: PreQualificationRequest.IPreLoanAdd = request.payload as any;
 
-                const data = await PreQualificationService.addPreQualifiedBanks(payload, userData);
-                return UniversalFunction.sendSuccess(Constant.STATUS_MSG.SUCCESS.S201.CREATED, {});
-            } catch (error) {
-                UniversalFunctions.consolelog(error, 'error', true);
-                return (UniversalFunction.sendError(error));
-            }
-        },
-        options: {
-            description: 'user prqualification Bank ',
-            tags: ['api', 'anonymous', 'user', 'bank', 'add', 'prequalification'],
-            auth: 'UserAuth',
-            validate: {
-                payload: {
-                    preQualificationId: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
-                    property: Joi.object().keys({
-                        value: Joi.number().min(50000),
-                        type: Joi.string().valid([
-                            LOAN_PROPERTY_TYPES.APARTMENT.value,
-                            LOAN_PROPERTY_TYPES.CONDOMINIUM.value,
-                            LOAN_PROPERTY_TYPES.HOUSE_LOT.value,
-                            LOAN_PROPERTY_TYPES.TOWNHOUSE.value,
-                            LOAN_PROPERTY_TYPES.VACANT_LOT.value,
-                        ]).required(),
-                        status: Joi.string().valid([
-                            LOAN_PROPERTY_STATUS.FORECLOSED.value,
-                            LOAN_PROPERTY_STATUS.REFINANCING.value,
-                            LOAN_PROPERTY_STATUS.PRE_SELLING.value,
-                            LOAN_PROPERTY_STATUS.READY_FOR_OCCUPANCY.value,
-                            LOAN_PROPERTY_STATUS.RESELLING.value,
-                            LOAN_PROPERTY_STATUS.FORECLOSED,
-                        ]).required(),
-                        developer: Joi.string(),
-                    }),
+    //             const data = await PreQualificationService.addPreQualifiedBanks(payload, userData);
+    //             return UniversalFunction.sendSuccess(Constant.STATUS_MSG.SUCCESS.S201.CREATED, {});
+    //         } catch (error) {
+    //             UniversalFunctions.consolelog(error, 'error', true);
+    //             return (UniversalFunction.sendError(error));
+    //         }
+    //     },
+    //     options: {
+    //         description: 'user prqualification Bank ',
+    //         tags: ['api', 'anonymous', 'user', 'bank', 'add', 'prequalification'],
+    //         auth: 'UserAuth',
+    //         validate: {
+    //             payload: {
+    //                 preQualificationId: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+    //                 property: Joi.object().keys({
+    //                     value: Joi.number().min(50000),
+    //                     type: Joi.string().valid([
+    //                         LOAN_PROPERTY_TYPES.APARTMENT.value,
+    //                         LOAN_PROPERTY_TYPES.CONDOMINIUM.value,
+    //                         LOAN_PROPERTY_TYPES.HOUSE_LOT.value,
+    //                         LOAN_PROPERTY_TYPES.TOWNHOUSE.value,
+    //                         LOAN_PROPERTY_TYPES.VACANT_LOT.value,
+    //                     ]).required(),
+    //                     status: Joi.string().valid([
+    //                         LOAN_PROPERTY_STATUS.FORECLOSED.value,
+    //                         LOAN_PROPERTY_STATUS.REFINANCING.value,
+    //                         LOAN_PROPERTY_STATUS.PRE_SELLING.value,
+    //                         LOAN_PROPERTY_STATUS.READY_FOR_OCCUPANCY.value,
+    //                         LOAN_PROPERTY_STATUS.RESELLING.value,
+    //                         LOAN_PROPERTY_STATUS.FORECLOSED,
+    //                     ]).required(),
+    //                     developer: Joi.string(),
+    //                 }),
 
-                    work: Joi.object().keys({
-                        type: Joi.string().valid([
-                            EMPLOYMENT_TYPE.BPO.value,
-                            EMPLOYMENT_TYPE.GOVT.value,
-                            EMPLOYMENT_TYPE.OFW.value,
-                            EMPLOYMENT_TYPE.PRIVATE.value,
-                            EMPLOYMENT_TYPE.PROFESSIONAL.value,
-                            EMPLOYMENT_TYPE.SELF.value,
-                        ]).required(),
+    //                 work: Joi.object().keys({
+    //                     type: Joi.string().valid([
+    //                         EMPLOYMENT_TYPE.BPO.value,
+    //                         EMPLOYMENT_TYPE.GOVT.value,
+    //                         EMPLOYMENT_TYPE.OFW.value,
+    //                         EMPLOYMENT_TYPE.PRIVATE.value,
+    //                         EMPLOYMENT_TYPE.PROFESSIONAL.value,
+    //                         EMPLOYMENT_TYPE.SELF.value,
+    //                     ]).required(),
 
-                        rank: Joi.string().valid([
-                            EMPLOYMENT_RANK.ASSISSTANT_VICE_PRESIDENT.value,
-                            EMPLOYMENT_RANK.ASSISTANT_MANAGER.value,
-                            EMPLOYMENT_RANK.CHAIRMAN.value,
-                            EMPLOYMENT_RANK.CHIEF_EXECUTIVE_OFFICER.value,
-                            EMPLOYMENT_RANK.CLERK.value,
-                            EMPLOYMENT_RANK.DIRECTOR.value,
-                            EMPLOYMENT_RANK.EXECUTIVE_VICE_PRESIDENT.value,
-                            EMPLOYMENT_RANK.FIRST_VICE_PRESIDENT.value,
-                            EMPLOYMENT_RANK.GENERAL_EMPLOYEE.value,
-                            EMPLOYMENT_RANK.MANAGER.value,
-                            EMPLOYMENT_RANK.NON_PROFESIONNAL.value,
-                            EMPLOYMENT_RANK.OWNER.value,
-                            EMPLOYMENT_RANK.PRESIDENT.value,
-                            EMPLOYMENT_RANK.PROFESSIONAL.value,
-                            EMPLOYMENT_RANK.RANK_FILE.value,
-                            EMPLOYMENT_RANK.SENIOR_ASSISTANT_MANAGER.value,
-                            EMPLOYMENT_RANK.SENIOR_ASSISTANT_VICE_PRESIDENT.value,
-                            EMPLOYMENT_RANK.SENIOR_MANAGER.value,
-                            EMPLOYMENT_RANK.SENIOR_VICE_PRESIDENT.value,
-                            EMPLOYMENT_RANK.SUPERVISOR.value,
-                            EMPLOYMENT_RANK.VICE_PRESIDENT.value,
-                        ]).required(),
+    //                     rank: Joi.string().valid([
+    //                         EMPLOYMENT_RANK.ASSISSTANT_VICE_PRESIDENT.value,
+    //                         EMPLOYMENT_RANK.ASSISTANT_MANAGER.value,
+    //                         EMPLOYMENT_RANK.CHAIRMAN.value,
+    //                         EMPLOYMENT_RANK.CHIEF_EXECUTIVE_OFFICER.value,
+    //                         EMPLOYMENT_RANK.CLERK.value,
+    //                         EMPLOYMENT_RANK.DIRECTOR.value,
+    //                         EMPLOYMENT_RANK.EXECUTIVE_VICE_PRESIDENT.value,
+    //                         EMPLOYMENT_RANK.FIRST_VICE_PRESIDENT.value,
+    //                         EMPLOYMENT_RANK.GENERAL_EMPLOYEE.value,
+    //                         EMPLOYMENT_RANK.MANAGER.value,
+    //                         EMPLOYMENT_RANK.NON_PROFESIONNAL.value,
+    //                         EMPLOYMENT_RANK.OWNER.value,
+    //                         EMPLOYMENT_RANK.PRESIDENT.value,
+    //                         EMPLOYMENT_RANK.PROFESSIONAL.value,
+    //                         EMPLOYMENT_RANK.RANK_FILE.value,
+    //                         EMPLOYMENT_RANK.SENIOR_ASSISTANT_MANAGER.value,
+    //                         EMPLOYMENT_RANK.SENIOR_ASSISTANT_VICE_PRESIDENT.value,
+    //                         EMPLOYMENT_RANK.SENIOR_MANAGER.value,
+    //                         EMPLOYMENT_RANK.SENIOR_VICE_PRESIDENT.value,
+    //                         EMPLOYMENT_RANK.SUPERVISOR.value,
+    //                         EMPLOYMENT_RANK.VICE_PRESIDENT.value,
+    //                     ]).required(),
 
-                        tenure: Joi.string().valid(Object.keys(EMPLOYMENT_TENURE)),
-                        income: Joi.number().min(25000),
-                    }),
+    //                     tenure: Joi.string().valid(Object.keys(EMPLOYMENT_TENURE)),
+    //                     income: Joi.number().min(25000),
+    //                 }),
 
-                    other: Joi.object().keys({
-                        age: Joi.number().min(21).max(65),
-                        nationality: Joi.string().valid([
-                            NATIONALITY.FILIPINO.value,
-                            NATIONALITY.FOREIGNER.value,
-                        ]),
-                        localVisa: Joi.boolean(),
-                        creditCard: Joi.object().keys({
-                            status: Joi.string().valid([
-                                CREDIT_CARD_STATUS.YES.value,
-                                CREDIT_CARD_STATUS.NO.value,
-                                CREDIT_CARD_STATUS.NOT_NOW.value,
-                            ]),
-                            limit: Joi.number(),
-                            cancelled: Joi.boolean(),
-                        }),
-                        prevLoans: Joi.object().keys({
-                            status: Joi.boolean(),
-                            monthlyTotal: Joi.number(),
-                            remainingTotal: Joi.number(),
-                        }),
-                        otherIncome: Joi.object().keys({
-                            status: Joi.boolean(),
-                            monthlyIncome: Joi.number(),
-                        }),
-                        married: {
-                            status: Joi.boolean(),
-                            spouseMonthlyIncome: Joi.number(),
-                        },
-                        coBorrower: {
-                            status: Joi.boolean(),
-                            coBorrowerMonthlyIncome: Joi.number(),
-                        },
-                    }),
+    //                 other: Joi.object().keys({
+    //                     age: Joi.number().min(21).max(65),
+    //                     nationality: Joi.string().valid([
+    //                         NATIONALITY.FILIPINO.value,
+    //                         NATIONALITY.FOREIGNER.value,
+    //                     ]),
+    //                     localVisa: Joi.boolean(),
+    //                     creditCard: Joi.object().keys({
+    //                         status: Joi.string().valid([
+    //                             CREDIT_CARD_STATUS.YES.value,
+    //                             CREDIT_CARD_STATUS.NO.value,
+    //                             CREDIT_CARD_STATUS.NOT_NOW.value,
+    //                         ]),
+    //                         limit: Joi.number(),
+    //                         cancelled: Joi.boolean(),
+    //                     }),
+    //                     prevLoans: Joi.object().keys({
+    //                         status: Joi.boolean(),
+    //                         monthlyTotal: Joi.number(),
+    //                         remainingTotal: Joi.number(),
+    //                     }),
+    //                     otherIncome: Joi.object().keys({
+    //                         status: Joi.boolean(),
+    //                         monthlyIncome: Joi.number(),
+    //                     }),
+    //                     married: {
+    //                         status: Joi.boolean(),
+    //                         spouseMonthlyIncome: Joi.number(),
+    //                     },
+    //                     coBorrower: {
+    //                         status: Joi.boolean(),
+    //                         coBorrowerMonthlyIncome: Joi.number(),
+    //                     },
+    //                 }),
 
-                    loan: Joi.object().keys({
-                        type: Joi.string(),
-                        term: Joi.number(),
-                        percent: Joi.number(),
-                        amount: Joi.number(),
-                        fixingPeriod: Joi.number(),
-                    }),
+    //                 loan: Joi.object().keys({
+    //                     type: Joi.string(),
+    //                     term: Joi.number(),
+    //                     percent: Joi.number(),
+    //                     amount: Joi.number(),
+    //                     fixingPeriod: Joi.number(),
+    //                 }),
 
-                    // prequalifiedBanks: Joi.array().items(objectSchema),
-                },
-                headers: UniversalFunctions.authorizationHeaderObj,
-                failAction: UniversalFunctions.failActionFunction,
-            },
-        },
-    },
+    //                 // prequalifiedBanks: Joi.array().items(objectSchema),
+    //             },
+    //             headers: UniversalFunctions.authorizationHeaderObj,
+    //             failAction: UniversalFunctions.failActionFunction,
+    //         },
+    //     },
+    // },
     /**
      * @description user get preQulification loan list
      */
