1d3097a7a3347d12e80ee60d08c93029c889d9eb
Shubham S <shubham.maheshwari@appinventiv.com>
2020-05-06 20:41:30 +0530

    update

--- src/routes/loans/loan.routes.ts
+++ src/routes/loans/loan.routes.ts
@@ -169,7 +169,7 @@ export let loanRoute: ServerRoute[] = [
 							LoanConstant.LOAN_TYPES.PURCHASE_OF_PROPERTY.value,
 							LoanConstant.LOAN_TYPES.REFINANCING_LOAN.value,
 							LoanConstant.LOAN_TYPES.RENOVATION.value,
-							LoanConstant.LOAN_TYPES.NEW_CONSTRUCTION.value,
+							// LoanConstant.LOAN_TYPES.NEW_CONSTRUCTION.value,
 
 						]),
 						loanPercent: Joi.number(),
@@ -589,7 +589,7 @@ export let loanRoute: ServerRoute[] = [
 							LoanConstant.LOAN_TYPES.PURCHASE_OF_PROPERTY.value,
 							LoanConstant.LOAN_TYPES.REFINANCING_LOAN.value,
 							LoanConstant.LOAN_TYPES.RENOVATION.value,
-							LoanConstant.LOAN_TYPES.NEW_CONSTRUCTION.value,
+							// LoanConstant.LOAN_TYPES.NEW_CONSTRUCTION.value,
 						]),
 						loanPercent: Joi.number(),
 						loanAmount: Joi.number(),
