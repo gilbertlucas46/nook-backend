be72caecbf8f5fa28a19baf581a7b4fea83cf90a
Shubham S <shubham.maheshwari@appinventiv.com>
2020-05-12 22:23:13 +0530

    update

--- src/entity/loan/prequalification.entity.ts
+++ src/entity/loan/prequalification.entity.ts
@@ -171,8 +171,12 @@ class PreLoanEntities extends BaseEntity {
                         processingTime: 'As fast as 5 working days upon submission of complete documents',
                         interestRate: 1,
                         loanDuration: 1,
-                        totalLoanMonthly: { $add: [{ $divide: ['$numerator', '$denominator'] }, preLoanMonthlyAmount] },
-                        monthlyPayment: { $divide: ['$numerator', '$denominator'] },
+                        // totalLoanMonthly: { $add: [{ $divide: ['$numerator', '$denominator'] }, preLoanMonthlyAmount] },
+                        // monthlyPayment: { $divide: ['$numerator', '$denominator'] },
+                        totalLoanMonthly: { $round: [{ $add: [{ $divide: ['$numerator', '$denominator'] }, preLoanMonthlyAmount] }, 2] },
+                        monthlyPayment: {
+                            $round: [{ $divide: ['$numerator', '$denominator'] }, 2],
+                        },
                         totalLoanPayment: 1,
                         bankId: '$_id',
                         _id: 0,
@@ -194,7 +198,7 @@ class PreLoanEntities extends BaseEntity {
                 },
                 {
                     $addFields: {
-                        debtIncomePercentRatio: { $divide: [{ $multiply: ['$totalLoanMonthly', 100] }, totalMonthlyIncome] },
+                        debtIncomePercentRatio: { $round: [{ $divide: [{ $multiply: ['$totalLoanMonthly', 100] }, totalMonthlyIncome] }, 2] },
                     },
                 },
                 {
