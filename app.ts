2117bbb20a449f685cd6b8ca3712c39e82fff21a
Shubham S <shubham.maheshwari@appinventiv.com>
2020-05-21 17:38:23 +0530

    loan-pdf

--- app.ts
+++ app.ts
@@ -1,79 +1,151 @@
-import { Server } from 'hapi';
+// import { Server } from 'hapi';
+// import { Routes } from './src/routes';
+// import { plugins } from './src/plugins';
+// import * as Bootstrap from './src/utils/bootstrap';
+// import * as config from 'config';
+// const path = require('path');
+
+// // import * as Hsrc from "hapi";
+
+// // import * as Constant from './src/constants';
+// // import * as cron from 'node-cron';
+// // import * as ENTITY from './src/entity';
+// // import { ExpireServices, job1 } from './src/scheduler/propertyExpire';
+
+// // let env = (process.env.NODE_ENV) ? process.env.NODE_ENV : 'default';
+
+// const originArray: string[] = [
+// 	'http://localhost:4200',
+// 	'http://localhost:4201',
+// 	'http://localhost',
+// 	'https://nookdevangweb.appskeeper.com',
+// 	'http://nookdevangadmin.appskeeper.com',
+// 	'https://nookdevangadmin.appskeeper.com',
+
+// 	'http://nookstgangweb.appskeeper.com',
+// 	'https://nookstgangweb.appskeeper.com',
+// 	'http://nookstgangadmin.appskeeper.com',
+// 	'https://nookstgangadmin.appskeeper.com',
+
+// 	'http://nookqaangweb.appskeeper.com',
+// 	'https://nookqaangweb.appskeeper.com',
+// 	'http://nookqaangadmin.appskeeper.com',
+// 	'https://nookqaangadmin.appskeeper.com',
+// 	'http://10.10.6.249:4200',
+// 	'http://localhost:4200',
+// 	'http://10.10.7.255:4200',
+// 	'http://10.10.8.213:4200',
+// 	// 'http://nookdev.appskeeper.com/documentation#!',
+// ];
+
+// const server = new Server({
+// 	port: config.get('port'),
+// 	routes: {
+// 		cors: {
+// 			origin: ['*'], // originArray,
+// 			additionalHeaders: ['Accept', 'Access-Control-Allow-Origin', 'x-requested-with', 'Access-Control-Allow-Headers', 'api_key', 'Authorization', 'authorization', 'Content-Type', 'If-None-Match', 'platform'],
+// 			exposedHeaders: ['Accept'],
+// 			// maxAge: 60,
+// 			credentials: true,
+// 			headers: ['Authorization'],
+// 		},
+// 		files: {
+// 			relativeTo: path.join(process.cwd(), '/views')
+// 		},
+// 	},
+// });
+// console.log('process.cwd()process.cwd()', process.cwd());
+
+// const init = async () => {
+// 	await server.register(plugins);
+// 	Routes.push({
+// 		method: 'GET',
+// 		path: '/{param*}',
+// 		options: {
+// 			handler: {
+// 				directory: {
+// 					path: process.cwd() + '/src/views/',
+// 					// listing: false,
+// 				},
+// 			},
+// 		},
+// 	});
+
+// 	server.route(Routes);
+// 	await server.start();
+// 	const db = new Bootstrap.Bootstrap();
+// 	await db.bootstrap();
+// 	// ExpireServices.updateProperty();
+// };
+
+// init().then(_ => {
+// 	console.log(`Server running at: ${config.get('port')}`);
+// }).catch((err) => {
+// 	console.warn('Error while loading plugins : ');
+// 	console.error(err);
+// 	process.exit(0);
+// });
+
+//"mongodb://uchatdev_usr:bN9XMhccGvS9WwSh@uchatdev.appskeeper.com:27200/uchatdev_db"
+//"mongodb://uchatdev_usr:bN9XMhccGvS9WwSh@uchatdev.appskeeper.com:27200/uchatdev_db"
+
+'use strict';
+//Internal Dependencies
+import * as Hsrc from 'hapi';
 import { Routes } from './src/routes';
 import { plugins } from './src/plugins';
+import * as Constant from './src/constants';
 import * as Bootstrap from './src/utils/bootstrap';
-import * as config from 'config';
-// import * as Constant from './src/constants';
-// import * as cron from 'node-cron';
-// import * as ENTITY from './src/entity';
-// import { ExpireServices, job1 } from './src/scheduler/propertyExpire';
-
-// let env = (process.env.NODE_ENV) ? process.env.NODE_ENV : 'default';
-
-const originArray: string[] = [
-	'http://localhost:4200',
-	'http://localhost:4201',
-	'http://localhost',
-	'https://nookdevangweb.appskeeper.com',
-	'http://nookdevangadmin.appskeeper.com',
-	'https://nookdevangadmin.appskeeper.com',
-
-	'http://nookstgangweb.appskeeper.com',
-	'https://nookstgangweb.appskeeper.com',
-	'http://nookstgangadmin.appskeeper.com',
-	'https://nookstgangadmin.appskeeper.com',
-
-	'http://nookqaangweb.appskeeper.com',
-	'https://nookqaangweb.appskeeper.com',
-	'http://nookqaangadmin.appskeeper.com',
-	'https://nookqaangadmin.appskeeper.com',
-	'http://10.10.6.249:4200',
-	'http://localhost:4200',
-	'http://10.10.7.255:4200',
-	'http://10.10.8.213:4200',
-	// 'http://nookdev.appskeeper.com/documentation#!',
-];
+import * as config from 'config'
+import { Server } from 'hapi';
 
 const server = new Server({
 	port: config.get('port'),
-	routes: {
-		cors: {
-			origin: ['*'], // originArray,
-			additionalHeaders: ['Accept', 'Access-Control-Allow-Origin', 'x-requested-with', 'Access-Control-Allow-Headers', 'api_key', 'Authorization', 'authorization', 'Content-Type', 'If-None-Match', 'platform'],
-			exposedHeaders: ['Accept'],
-			// maxAge: 60,
-			credentials: true,
-			headers: ['Authorization'],
-		},
-	},
+	routes: { cors: true },
 });
+console.log('process.cwd() process.cwd() ', process.cwd());
 
 const init = async () => {
 	await server.register(plugins);
-	Routes.push({
-		method: 'GET',
-		path: '/images/{path*}',
-		options: {
-			handler: {
-				directory: {
-					path: process.cwd() + '/src/views/images/',
-					listing: false,
+	try {
+		Routes.push(
+			{
+				method: 'GET',
+				path: '/src/views/loan/'.toString() + `{path*}`,//'/views/uploads/image/{path*}',
+				options: {
+					handler: {
+						directory: {
+							path: process.cwd() + '/src/views/loan'.toString(),
+							listing: false,
+						}
+					},
 				},
 			},
-		},
-	});
+			// {
+			// 	method: 'GET',
+			// 	path: config.get('basepath.url.csv').toString() + `{path*}`,
+			// 	options: {
+			// 		handler: {
+			// 			directory: {
+			// 				path: process.cwd() + config.get('basepath.url.csv').toString(),
+			// 				listing: false,
+			// 			}
+			// 		}
+			// 	}
+			// }
+		)
+		server.route(Routes);
 
-	server.route(Routes);
-	await server.start();
-	const db = new Bootstrap.Bootstrap();
-	await db.bootstrap();
-	// ExpireServices.updateProperty();
+
+		server.log('info', 'Plugins Loaded');
+		await server.start();
+		let db = new Bootstrap.Bootstrap();
+		await db.bootstrap();
+
+		console.log('Server running at:', 7362);
+	} catch (err) {
+		console.log('Error while loading plugins : ' + err);
+	}
 };
 
-init().then(_ => {
-	console.log(`Server running at: ${config.get('port')}`);
-}).catch((err) => {
-	console.warn('Error while loading plugins : ');
-	console.error(err);
-	process.exit(0);
-});
+init();
--- src/lib/mail.manager.ts
+++ src/lib/mail.manager.ts
@@ -195,11 +195,35 @@ export class MailManager {
 	async generateLoanApplicationform(params) {
 		try {
 			console.log('paramssssssssssss', params);
-			console.log('paramssssssssssss', params.personalInfo);
+			console.log('paramssssssssssss', params.personalInfo.spouseInfo);
 
-			// const checkObjectBlank = Object.keys(params['personalInfo']['coBorrowerInfo']).length === 0 ? false : true;
+			let { coBorrowerInfo, spouseInfo } = params['personalInfo'];
+			console.log('spouseInfospouseInfo', spouseInfo);
 
 
+
+			let checkObjectBlank;
+			if (coBorrowerInfo) {
+				checkObjectBlank = (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo.firstName) ? true : false
+				console.log('checkObjectBlankcheckObjectBlankcheckObjectBlank', checkObjectBlank);
+			} else {
+				coBorrowerInfo = {};
+			}
+			console.log("coBorrowerInfo['firstName']", coBorrowerInfo['firstName']);
+
+			// if (!spouseInfo) {
+			// 	spouseInfo = null
+			// }
+
+			// let { coBorrowerFullName, relationship }
+
+			console.log(':::::::::<>>>>>>>>>>>>>>>>>', (spouseInfo && spouseInfo['firstName']) ? spouseInfo['firstName'] + ' ' + spouseInfo['lastName'] : 'N/A');
+			// console.log("spouseInfo ? new Date(spouseInfo['birthDate']).toLocaleDateString() : 'N/A", spouseInfo ? new Date(spouseInfo['birthDate']).toLocaleDateString() : 'N/A');
+			// console.log("paraersonalInfo'][lastName", params['personalInfo']['firstName'] + '' + params['personalInfo']['middleName'] ? + params['personalInfo']['middleName'] + ' ' + params['personalInfo']['lastName']);
+			console.log('coBorrowerInfocoBorrowerInfo', coBorrowerInfo);
+			console.log('gggggggggg', params.employmentInfo['coBorrowerInfo']);
+
+			console.log('LLLLLLLLLLLLLLLLLLl', (params.employmentInfo && params.employmentInfo['coBorrowerInfo'] && params.employmentInfo['coBorrowerInfo']['tin']) ? params.employmentInfo.coBorrowerInfo['tin'] : 'N/A');
 
 			const htmlContent = await (new TemplateUtil(SERVER.TEMPLATE_PATH + 'loan-form.html'))
 				.compileFile({
@@ -215,30 +239,31 @@ export class MailManager {
 					mobileNo: params.contactInfo['mobileNumber'],
 					email: params.contactInfo['email'],
 
-					// spouseFullName: params['personalInfo']['spouseInfo']['firstName'] ? params['personalInfo']['spouseInfo']['firstName'] + ' ' + params['personalInfo']['spouseInfo']['lastName'] : 'N/A',
+					spouseFullName: (spouseInfo && spouseInfo['firstName']) ? spouseInfo['firstName'] + ' ' + spouseInfo['lastName'] : 'N/A',
 					motherMaidenName: params['personalInfo']['motherMaidenName'],
 					educationBackground: params['personalInfo']['educationBackground'],
-					// spouseBirthDate: params['personalInfo']['spouseInfo']['birthDate'] ? new Date(params['personalInfo']['spouseInfo']['birthDate']).toLocaleDateString() : 'N/A',
+					spouseBirthDate: (spouseInfo && spouseInfo['birthDate']) ? new Date(spouseInfo['birthDate']).toLocaleDateString() : 'N/A',
 					currentAddress: params.contactInfo['currentAddress']['address'],
 					// permannet address
 
 					homeOwnership: params.contactInfo['currentAddress']['homeOwnership'],
 
 					// CO-BORROWER’S INFORMATION
-					// isCoborrower: checkObjectBlank, //+ ' ' + params['personalInfo']['coBorrowerInfo']['middleName']
-					// coBorrowerFullName: params['personalInfo']['coBorrowerInfo']['firstName'] + ' ' + params['personalInfo']['coBorrowerInfo']['lastName'],
-					relationship: params['personalInfo']['coBorrowerInfo']['relationship'],
-					monthlyIncome: params['personalInfo']['coBorrowerInfo']['monthlyIncome'],
-					coBorrowerTIN: params['employmentInfo']['coBorrowerInfo']['tin'] ? params['employmentInfo']['coBorrowerInfo']['tin'] : 'N/A',
-					coBorrowerSSS: params['employmentInfo']['coBorrowerInfo']['sss'] ? params['employmentInfo']['coBorrowerInfo']['sss'] : 'N/A',
-					coBorrowerEmploymentType: params['employmentInfo']['coBorrowerInfo']['employmentType'] ? params['employmentInfo']['coBorrowerInfo']['employmentType'] : 'N/A',
-					coBorrowerEmploymentRank: params['employmentInfo']['coBorrowerInfo']['employmentRank'] ? params['employmentInfo']['coBorrowerInfo']['employmentRank'] : 'N/A',
-					coBorrowerEmploymentTenure: params['employmentInfo']['coBorrowerInfo']['employmentTenure'] ? params['employmentInfo']['coBorrowerInfo']['employmentTenure'] : 'N/A',
-					coBorrowerCompanyName: params['employmentInfo']['coBorrowerInfo']['companyName'] ? params['employmentInfo']['coBorrowerInfo']['companyName'] : 'N/A',
-					coBorrowerCompanyIndustry: params['employmentInfo']['coBorrowerInfo']['companyIndustry'] ? params['employmentInfo']['coBorrowerInfo']['companyIndustry'] : 'N/A',
-					coBorrowerOfficePhone: params['employmentInfo']['coBorrowerInfo']['officePhone'] ? params['employmentInfo']['coBorrowerInfo']['officePhone'] : 'N/A',
-					coBorrowerOfficeEmail: params['employmentInfo']['coBorrowerInfo']['officeEmail'] ? params['employmentInfo']['coBorrowerInfo']['officeEmail'] : 'N/A',
-					coBorrowerOfficeAddress: params['employmentInfo']['coBorrowerInfo']['officeAddress'] ? params['employmentInfo']['coBorrowerInfo']['officeAddress'] : 'N/A',
+					isCoborrower: checkObjectBlank, // + ' ' + params['personalInfo']['coBorrowerInfo']['middleName']
+					coBorrowerFullName: (coBorrowerInfo && coBorrowerInfo['firstName']) ? coBorrowerInfo['firstName'] + ' ' + coBorrowerInfo['lastName'] : 'N/A',
+					relationship: (coBorrowerInfo && coBorrowerInfo['relationship']) ? coBorrowerInfo['relationship'] : 'N/A',
+					// relationship: coBorrowerInfo ? coBorrowerInfo['relationship'] : 'N/A',
+					monthlyIncome: (coBorrowerInfo && coBorrowerInfo['monthlyIncome']) ? coBorrowerInfo['monthlyIncome'] : 'N/A',
+					coBorrowerTIN: (params.employmentInfo && params.employmentInfo['coBorrowerInfo'] && params.employmentInfo['coBorrowerInfo']['tin']) ? params.employmentInfo.coBorrowerInfo['tin'] : 'N/A',
+					coBorrowerSSS: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && coBorrowerInfo['sss']) ? params.employmentInfo.coBorrowerInfo['sss'] : 'N/A',
+					coBorrowerEmploymentType: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentType']) ? params.employmentInfo.coBorrowerInfo['employmentType'] : 'N/A',
+					coBorrowerEmploymentRank: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentRank']) ? params.employmentInfo.coBorrowerInfo['employmentRank'] : 'N/A',
+					coBorrowerEmploymentTenure: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentTenure']) ? params.employmentInfo.coBorrowerInfo['employmentTenure'] : 'N/A',
+					coBorrowerCompanyName: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['companyName']) ? params.employmentInfo.coBorrowerInfo['companyName'] : 'N/A',
+					coBorrowerCompanyIndustry: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['companyIndustry']) ? params.employmentInfo.coBorrowerInfo['companyIndustry'] : 'N/A',
+					coBorrowerOfficePhone: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['officePhone']) ? params.employmentInfo.coBorrowerInfo['officePhone'] : 'N/A',
+					coBorrowerOfficeEmail: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['officeEmail']) ? params.employmentInfo.coBorrowerInfo['officeEmail'] : 'N/A',
+					coBorrowerOfficeAddress: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['officeAddress']) ? params.employmentInfo.coBorrowerInfo['officeAddress'] : 'N/A',
 
 
 					// loan information
@@ -271,8 +296,8 @@ export class MailManager {
 					employmentRank: params['employmentInfo']['rank'],
 					employmentTenure: params['employmentInfo']['tenure'],
 					// grossMonthlyIncome:params['']   // to be done
-					tin: params['employmentInfo']['tin'],
-					sss: params['employmentInfo']['sss'],
+					tin: params['employmentInfo'] ? params['employmentInfo']['tin'] : 'N/A',
+					sss: params['employmentInfo'] ? params['employmentInfo']['tin'] : ['sss'],
 					companyName: params['employmentInfo']['companyName'],
 					companyIndustry: params['employmentInfo']['companyIndustry'],
 					officePhone: params['employmentInfo']['officePhone'],
@@ -315,7 +340,7 @@ export class MailManager {
 			console.log('htmlContenthtmlContenthtmlContent', typeof htmlContent);
 
 			const htmlData = new PdfGenerator();
-			// const a = await htmlData.test(htmlContent, params['referenceId']);
+			const a = await htmlData.test(htmlContent, params['referenceId']);
 			// console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa', a);
 
 			return htmlContent;
