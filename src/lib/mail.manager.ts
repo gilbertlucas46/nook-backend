1198f313e582dc80101ed661addfb33660e40387
Shubham S <shubham.maheshwari@appinventiv.com>
2020-05-25 23:20:02 +0530

    update

--- src/lib/mail.manager.ts
+++ src/lib/mail.manager.ts
@@ -235,7 +235,7 @@ export class MailManager {
 				.compileFile({
 					applicationId: params['referenceId'],
 					nookLogoUrl: config['host'] + '/images/nooklogo.png',
-					fullName: params['personalInfo']['firstName'] + ' ' + params['personalInfo']['lastName'],  // + params['personalInfo']['middleName'] ? params['personalInfo']['middleName'] : '' 
+					fullName: params['personalInfo']['firstName'] + params['personalInfo']['middleName'] ? params['personalInfo']['middleName'] : '' + ' ' + params['personalInfo']['lastName'],  // + params['personalInfo']['middleName'] ? params['personalInfo']['middleName'] : '' 
 					civilStatus: params['personalInfo']['civilStatus'],
 					sex: params['personalInfo']['gender'],
 					citizenship: params['personalInfo']['nationality'],
@@ -261,10 +261,10 @@ export class MailManager {
 					// relationship: coBorrowerInfo ? coBorrowerInfo['relationship'] : 'N/A',
 					monthlyIncome: (coBorrowerInfo && coBorrowerInfo['monthlyIncome']) ? coBorrowerInfo['monthlyIncome'] : 'N/A',
 					coBorrowerTIN: (params.employmentInfo && params.employmentInfo['coBorrowerInfo'] && params.employmentInfo['coBorrowerInfo']['tin']) ? params.employmentInfo.coBorrowerInfo['tin'] : 'N/A',
-					coBorrowerSSS: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && coBorrowerInfo['sss']) ? params.employmentInfo.coBorrowerInfo['sss'] : 'N/A',
+					coBorrowerSSS: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['sss']) ? params.employmentInfo.coBorrowerInfo['sss'] : 'N/A',
 					coBorrowerEmploymentType: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentType']) ? params.employmentInfo.coBorrowerInfo['employmentType'] : 'N/A',
 					coBorrowerEmploymentRank: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentRank']) ? params.employmentInfo.coBorrowerInfo['employmentRank'] : 'N/A',
-					coBorrowerEmploymentTenure: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentTenure']) ? params.employmentInfo.coBorrowerInfo['employmentTenure'] : 'N/A',
+					coBorrowerEmploymentTenure: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['employmentTenure']) ? params.employmentInfo.coBorrowerInfo['employmentTenure'] + ' ' + 'year' : 'N/A',
 					coBorrowerCompanyName: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['companyName']) ? params.employmentInfo.coBorrowerInfo['companyName'] : 'N/A',
 					coBorrowerCompanyIndustry: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['companyIndustry']) ? params.employmentInfo.coBorrowerInfo['companyIndustry'] : 'N/A',
 					coBorrowerOfficePhone: (params.employmentInfo && params.employmentInfo.coBorrowerInfo && params.employmentInfo.coBorrowerInfo['officePhone']) ? params.employmentInfo.coBorrowerInfo['officePhone'] : 'N/A',
@@ -274,19 +274,19 @@ export class MailManager {
 
 					// loan information
 					bankName: params['bankInfo']['bankName'],
-					loanTerm: params['loanDetails']['loanTerm'],
-					fixedPeriod: params['loanDetails']['fixedPeriod'],
+					loanTerm: params['loanDetails']['loanTerm'] + ' ' + 'year',
+					fixedPeriod: params['loanDetails']['fixedPeriod'] + ' ' + 'year',
 					loanPercent: params['loanDetails']['loanPercent'],
 					loanAmount: params['loanDetails']['loanAmount'],
-					interestRate: params['loanDetails']['rate:'],
+					interestRate: params['loanDetails']['rate'],
 					loanType: params['loanDetails']['loanType'],
 					// Loan Total PaymentA (Php): params['loanDetails']['loanAmount'],
-					monthlyRepayment: 'N/A',
+					monthlyRepayment: params['loanDetails']['monthlyRepayment'],
 					loanTotalPayment: 'N/A',
 
 
 					// COLLATERAL INFORMATION
-					propertyValue: params['propertyInfo']['value'],
+					propertyValue: params['propertyInfo']['value'] + 'php',
 					propertyType: params['propertyInfo']['type'],
 					propertyStatus: params['propertyInfo']['status'],
 					propertyDeveloper: params['propertyInfo']['developer'] ? params['propertyInfo']['developer'] : 'N/A',
@@ -312,21 +312,22 @@ export class MailManager {
 
 
 					// LOANS AND CREDIT CARDS
-					otherLoan: '',
-					totalMonthlyPayments: '',
-					//  Total Remaining Balance (Php)
+					otherLoan: params['personalInfo'] && params['personalInfo']['prevLoans'] && params['personalInfo']['prevLoans']['status'] ? params['personalInfo']['prevLoans']['status'] : 'N/A',
+					totalMonthlyPayments: params['personalInfo'] && params['personalInfo']['prevLoans'] && params['personalInfo']['prevLoans']['monthlyTotal'] ? params['personalInfo']['prevLoans']['monthlyTotal'] : 'N/A',
+					totalRemainingBalance: params['personalInfo'] && params['personalInfo']['prevLoans'] && params['personalInfo']['prevLoans']['remainingTotal'] ? params['personalInfo']['prevLoans']['remainingTotal'] : 'N/A',
 					//
 					// Credit Card Limit (Php)
 
 
-					creditCard: '', // Credit Card Issuing Bank? 					,
+					creditCardStatus: params['personalInfo'] && params['personalInfo']['creditCard'] && params['personalInfo']['creditCard']['status'] === 'YES' ? 'Yes ,active credit card' : 'No', // Credit Card Issuing Bank? 					,
+					creditCardCancelled: params['personalInfo'] && params['personalInfo']['creditCard'] && params['personalInfo']['creditCard']['cancelled'] ? params['personalInfo']['creditCard']['cancelled'] : 'N/A',
 					// Total Monthly Payments (Php)
 
 					// Total Remaining Balance (Php)
 
-					// Credit Card Limit (Php)
+					creditCardLimit: params['personalInfo'] && params['personalInfo']['creditCard'] && params['personalInfo']['creditCard']['limit'] ? params['personalInfo']['creditCard']['limit'] : 'N/A',
 
-					// Credit Card Issuing Bank?
+					creaditCardIssuingBank: 'N/A',
 
 					//  DEPENDENTS AND REFERENCES
 
@@ -356,7 +357,7 @@ export class MailManager {
 					helpCenter: config.get('homePage') + '/help-center',
 				});
 			// console.log('htmlContenthtmlContenthtmlContent', htmlContent);
-			console.log('htmlContenthtmlContenthtmlContent', typeof htmlContent);
+			console.log('htmlContenthtmlContenthtmlContent', htmlContent typeof htmlContent);
 
 			// const htmlData = new PdfGenerator();
 			const a = await pdfClass.test(htmlContent, params['referenceId']);
