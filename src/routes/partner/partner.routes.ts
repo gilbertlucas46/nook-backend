c0b2766d6200b6aa11a4d7d49861db5c9f94e0f0
Shubham S <shubham.maheshwari@appinventiv.com>
2020-06-03 16:25:44 +0530

    update

--- src/routes/partner/partner.routes.ts
+++ src/routes/partner/partner.routes.ts
@@ -105,7 +105,8 @@ export let partnerRoutes: ServerRoute[] = [
                 console.log('datadata>>>>>>>>>>>>>>>', data);
                 if (data) {
                     console.log('>>>>>>>>>>>>>>>>>>>>', 'localhost:4200' + '?pre-qualification=' + data['shortId']);
-                    return h.redirect('localhost:4200' + '?pre-qualification=' + data['shortId']);
+                    // return 'localhost:4200' + '?pre-qualification=' + data['shortId'];
+                    return h.redirect(config.get('homePage')); //  + '?pre-qualification=' + data['shortId']);
                 } else {
                     return h.redirect(config.get('homePage'));
                 }
