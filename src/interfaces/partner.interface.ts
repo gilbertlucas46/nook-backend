b0097fb0f14fc145276275d746bf88fde0b26a57
Shubham S <shubham.maheshwari@appinventiv.com>
2020-06-08 19:03:44 +0530

    update

--- src/interfaces/partner.interface.ts
+++ src/interfaces/partner.interface.ts
@@ -1,6 +1,7 @@
 export namespace PartnerAdminRequest {
 
     export interface CreatePartner {
+        webUrl: string;
         logoUrl: string;
         name: string;
         displayName?: string;
@@ -9,6 +10,16 @@ export namespace PartnerAdminRequest {
     export interface UpdateStatus {
         partnerSid: string;
         status: string;
+    }
+
+
+    export interface UpdatePartner {
+        partnerSid: string;
+        webUrl: string;
+        logoUrl: string;
+        name: string;
+        displayName?: string;
+
     }
 
 }
