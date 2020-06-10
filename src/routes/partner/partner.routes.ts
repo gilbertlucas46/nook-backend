91da29380d42678b567ec82608222a2966341f40
Shubham S <shubham.maheshwari@appinventiv.com>
2020-06-10 15:45:28 +0530

    update

--- src/routes/partner/partner.routes.ts
+++ src/routes/partner/partner.routes.ts
@@ -253,4 +253,43 @@ export let partnerRoutes: ServerRoute[] = [
         },
     },
 
+
+    {
+        method: 'GET',
+        path: '/v1/admin/partner/{partnerSid}',
+        handler: async (request, h) => {
+            try {
+                // const userData = request.auth && request.auth.credentials && (request.auth.credentials as any).adminData;
+                const payload = request.params as any;
+
+                const data = await PartnerService.adminGetPartnerInfo(payload);
+                return (UniversalFunctions.sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.DEFAULT, data));
+            } catch (error) {
+                UniversalFunctions.consolelog('error', error, true);
+                return (UniversalFunctions.sendError(error));
+            }
+        },
+        options: {
+            description: 'info of the partner application for admin',
+            tags: ['api', 'anonymous', 'partner', 'admin', 'info'],
+            auth: 'AdminAuth',
+            validate: {
+                params: {
+                    partnerSid: Joi.string().required(),
+                },
+                // payload: {
+                //     logoUrl: Joi.string().uri().required(),
+                //     name: Joi.string().required(),
+                //     displayName: Joi.string(),
+                // },
+                headers: UniversalFunctions.authorizationHeaderObj,
+                failAction: UniversalFunctions.failActionFunction,
+            },
+            plugins: {
+                'hapi-swagger': {
+                    responseMessages: Constant.swaggerDefaultResponseMessages,
+                },
+            },
+        },
+    },
 ];
