b55e386e51788214051094f7675ca96e601abbf5
shubham.maheshwari@appinventiv.com <shubham.maheshwari@appinventiv.com>
2020-07-11 23:54:37 +0530

    remove code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,17 @@
 import { ServerRoute } from 'hapi';
 
 import { userRoute } from './user/user.routes';
-// import { propertyRoute } from './property/property.routes';
 import { adminProfileRoute } from './admin/admin.routes';
 import { subAdminRoutes } from './admin/sub.admin.routes';
 import { contentRoutes } from './content/content.routes';
-// import { enquiryRoutes } from './enquiry/enquiry.routes';
 import { articleRoutes } from './article/article.routes';
 import { cityRoutes } from './city/city.routes';
-// import { agentRoute } from './user/agent.routes';
 import { helpCenterRoute } from './helpCenter/helpCenter.routes';
-// import { savedProperty } from './property/savedProperty.routes';
 import { loanRoute } from './loans/loan.routes';
 import { preloanRoute } from './loans/preloan.routes';
-// import { transactionRoute } from './transaction/transaction.routes';
 import { loanReferral } from './referral/loanReferral.routes';
 import { searchRoutes } from './search.routes';
-// import { subscriptionRoute } from './subscription/subscription.routes';
 import { adminUserRoutes } from './admin/admin.user.routes';
-// import { adminProperty } from './admin/admin.property.routes';
 import { preQualificationroutes } from './preQualification/preQualification.routes';
 import { partnerRoutes } from './partner/partner.routes';
 import { helpCenterCategoryRoutes } from './helpCenter/helpcenter.categories.routes';
@@ -26,28 +19,20 @@ import { helpCenterCategoryRoutes } from './helpCenter/helpcenter.categories.rou
 // const arr = [];
 export let Routes: ServerRoute[] = [
 	...userRoute,
-	// ...propertyRoute,
 	...adminProfileRoute,
 	...contentRoutes,
-	// ...enquiryRoutes,
 	...articleRoutes,
 	...cityRoutes,
-	// ...agentRoute,
 	...helpCenterRoute,
-	// ...savedProperty,
 	...subAdminRoutes,
 	...loanRoute,
 	...preloanRoute,
-	// ...transactionRoute,
 	...loanReferral,
 	...searchRoutes,
-	// ...subscriptionRoute,
 	...adminUserRoutes,
 	...preQualificationroutes,
 	...partnerRoutes,
 	...helpCenterCategoryRoutes,
-
-	// ...adminProperty,
 	// {
 	// 	method: ['*'],
 	// 	path: '/{any*}',
--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -82,35 +82,6 @@ export let userRoute: ServerRoute[] = [
 			},
 		},
 	},
-	// /**
-	//  * @description:get user's property via id
-	//  */
-	// {
-	// 	method: 'GET',
-	// 	path: '/v1/user/property/{_id}',
-	// 	async handler(request, h) {
-	// 		try {
-	// 			const userData = request.auth && request.auth.credentials && (request.auth.credentials as any).userData;
-	// 			const payload: PropertyRequest.PropertyDetail = request.params as any;
-	// 			const propertyDetail = await UserService.propertyDetail(payload, userData);
-	// 			return (UniversalFunctions.sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.DEFAULT, propertyDetail));
-	// 		} catch (error) {
-	// 			return (UniversalFunctions.sendError(error));
-	// 		}
-	// 	},
-	// 	options: {
-	// 		description: 'get detail of property ',
-	// 		tags: ['api', 'anonymous', 'user', 'register'],
-	// 		auth: 'DoubleAuth',
-	// 		validate: {
-	// 			params: {
-	// 				_id: Joi.string().trim().required(),
-	// 			},
-	// 			headers: UniversalFunctions.authorizationHeaderObj,
-	// 			failAction: UniversalFunctions.failActionFunction,
-	// 		},
-	// 	},
-	// },
 	/**
 	 *
 	 * @description: forget passsword to send the link over mail
@@ -378,101 +349,6 @@ export let userRoute: ServerRoute[] = [
 			},
 		},
 	},
-	// /**
-	//  * @description user all property except the current property
-	//  */
-	// {
-	// 	method: 'GET',
-	// 	path: '/v1/user/suggested-property',
-	// 	async handler(request, h) {
-	// 		try {
-	// 			const payload: PropertyRequest.UserProperty = request.query as any;
-	// 			const propertyDetail = await UserService.userProperty(payload);
-	// 			return (UniversalFunctions.sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.DEFAULT, propertyDetail));
-	// 		} catch (error) {
-	// 			UniversalFunctions.consolelog(error, 'error', true);
-	// 			return (UniversalFunctions.sendError(error));
-	// 		}
-	// 	},
-	// 	options: {
-	// 		description: 'get user usggested-property ',
-	// 		tags: ['api', 'anonymous', 'user', 'register'],
-	// 		auth: 'DoubleAuth',
-	// 		validate: {
-	// 			query: {
-	// 				propertyType: Joi.number().valid([
-	// 					Constant.DATABASE.PROPERTY_STATUS.ACTIVE.NUMBER,
-	// 					Constant.DATABASE.PROPERTY_STATUS.DRAFT.NUMBER,
-	// 					Constant.DATABASE.PROPERTY_STATUS.EXPIRED.NUMBER,
-	// 					Constant.DATABASE.PROPERTY_STATUS.PENDING.NUMBER,
-	// 					Constant.DATABASE.PROPERTY_STATUS.SOLD_RENTED.NUMBER,
-	// 					Constant.DATABASE.PROPERTY_ACTIONS.ISFEATURED.NUMBER,
-	// 				]).default(Constant.DATABASE.PROPERTY_STATUS.ACTIVE.NUMBER),
-	// 				page: Joi.number(),
-	// 				limit: Joi.number(),
-	// 				sortType: Joi.number().valid([
-	// 					Constant.ENUM.SORT_TYPE,
-	// 				]),
-	// 				propertyFor: Joi.number().valid([
-	// 					Constant.DATABASE.PROPERTY_FOR.RENT.NUMBER,
-	// 					Constant.DATABASE.PROPERTY_FOR.SALE.NUMBER,
-	// 				]),
-	// 				sortBy: Joi.string().valid(['price', 'date', 'isFeatured']),
-	// 				propertyId: Joi.string().trim(),
-	// 				userId: Joi.string().trim().regex(/^[0-9a-fA-F]{24}$/),
-	// 			},
-
-	// 			headers: UniversalFunctions.authorizationHeaderObj,
-	// 			failAction: UniversalFunctions.failActionFunction,
-	// 		},
-	// 		plugins: {
-	// 			'hapi-swagger': {
-	// 				responseMessages: Constant.swaggerDefaultResponseMessages,
-	// 			},
-	// 		},
-	// 	},
-	// },
-	/**
-	 * @description: update user Account
-	 */
-	// {
-	// 	method: 'PATCH',
-	// 	path: '/v1/user/update-account',
-	// 	async handler(request, h) {
-	// 		try {
-	// 			const userData = request.auth && request.auth.credentials && request.auth.credentials['userData'];
-	// 			const payload = request.payload as UserRequest.UpdateAccount;
-	// 			const propertyDetail = await UserService.updateAccount(payload, userData);
-	// 			const userResponse = UniversalFunctions.formatUserData(propertyDetail);
-	// 			return (UniversalFunctions.sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.DEFAULT, userResponse));
-	// 		} catch (error) {
-	// 			UniversalFunctions.consolelog(error, 'error', true);
-	// 			return (UniversalFunctions.sendError(error));
-	// 		}
-	// 	},
-	// 	options: {
-	// 		description: 'update account type ',
-	// 		tags: ['api', 'anonymous', 'user', 'updateAccount'],
-	// 		auth: 'UserAuth',
-	// 		validate: {
-	// 			payload: {
-	// 				// userType: Joi.string().trim().valid([
-	// 				// 	Constant.DATABASE.USER_TYPE.AGENT.TYPE,
-	// 				// 	Constant.DATABASE.USER_TYPE.TENANT.TYPE,
-	// 				// 	Constant.DATABASE.USER_TYPE.OWNER.TYPE,
-	// 				// ]),
-	// 			},
-	// 			headers: UniversalFunctions.authorizationHeaderObj,
-	// 			failAction: UniversalFunctions.failActionFunction,
-	// 		},
-	// 		plugins: {
-	// 			'hapi-swagger': {
-	// 				responseMessages: Constant.swaggerDefaultResponseMessages,
-	// 			},
-	// 		},
-	// 	},
-	// },
-
 	{
 		method: 'PATCH',
 		path: '/v1/user/complete-registration',
@@ -506,11 +382,6 @@ export let userRoute: ServerRoute[] = [
 					countryCode: Joi.string().default('+63'),
 					partnerName: Joi.string(),
 					partnerId: Joi.string(),
-					// type: Joi.string().trim().valid([
-					// 	Constant.DATABASE.USER_TYPE.AGENT.TYPE,
-					// 	Constant.DATABASE.USER_TYPE.OWNER.TYPE,
-					// 	Constant.DATABASE.USER_TYPE.TENANT.TYPE,
-					// ]).required(),
 				},
 				headers: UniversalFunctions.authorizationHeaderObj,
 				failAction: UniversalFunctions.failActionFunction,
