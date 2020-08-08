35a6b3d4cb26fe6e6584c7cc15b0555ebc52f7f2
shubham <shubham.maheshwari@appinventiv.com>
2020-08-09 00:35:00 +0530

    resolved issue

--- src/controllers/admin/admin.user.controller.ts
+++ src/controllers/admin/admin.user.controller.ts
@@ -226,6 +226,20 @@ class AdminUserControllers {
             }
             const updatedUser = await ENTITY.UserE.updateOneEntity(criteria, payload);
             const userResponse = UniversalFunctions.formatUserData(updatedUser);
+            if (config.get['environment'] === 'production') {
+                // if (!isProfileCompleted) {
+                // convert document to data
+                const salesforceData = flattenObject(userResponse.toObject ? userResponse.toObject() : userResponse);
+                console.log('salesforceDatasalesforceData', salesforceData);
+                const request = {
+                    method: 'post',
+                    body: JSON.stringify(salesforceData),
+                };
+                await fetch(config.get('zapier_personUrl'), request);
+                await fetch(config.get('zapier_accountUrl'), request);
+            }
+
+
             return userResponse;
         }
         catch (error) {
