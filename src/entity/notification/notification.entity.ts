a396275be9526e7098f9020dd581b458cf8779d9
shivam.singh1_devs <shivam.singh1@appinventiv.com>
2022-03-07 11:33:52 +0530

    notification

--- src/entity/notification/notification.entity.ts
+++ src/entity/notification/notification.entity.ts
@@ -1,18 +1,51 @@
 import { BaseEntity } from "@src/entity/base/base.entity";
 import * as Constant from "@src/constants";
 import { Types } from "mongoose";
+import { LoanRequest } from "@src/interfaces/loan.interface";
+import * as utils from '@src/utils';
+import { NotificationRequest } from "@src/interfaces/notification.interface";
 
 class NotificationEntities extends BaseEntity {
   constructor() {
     super("Notification");
   }
-  async notificationList(payload) {
+  async notificationList(payload:NotificationRequest.INotificationList) {
     try {
-      const notificationList = await this.DAOManager.findAll(
-        "Notification",
-        {},
-        { referralId: 1, message: 1, isRead: 1, createdAt: 1 }
-      );
+      const { 
+        limit = Constant.SERVER.LIMIT,
+        page = 1,
+    } = payload;
+    const skip = (limit * (page - 1));
+
+    const paginateOptions = {
+        page: page || 1,
+        limit: limit || Constant.SERVER.LIMIT,
+    };
+    const matchPipeline = [
+      
+      {
+          $sort: {
+              _id: -1,
+          },
+      },
+      {
+          $project: {
+              _id: 1,
+              loanId:1,
+              message:1,
+              isRead:1,
+              notificationType:1,
+              createdAt:1
+
+          },
+      },
+  ];
+  const notificationList = await this.DAOManager.paginatePipeline(matchPipeline, paginateOptions, []).aggregate(this.modelName);
+      // const notificationList = await this.DAOManager.findAll(
+      //   "Notification",
+      //   {},
+      //   { referralId: 1, message: 1, isRead: 1, createdAt: 1 }
+      // );
       return notificationList;
     } catch (error) {
       return Promise.reject(error);
@@ -44,6 +77,31 @@ class NotificationEntities extends BaseEntity {
       return Promise.reject(error);
     }
   }
+   /**
+     * @description notification update
+     * 
+     */
+    async updateNotification(payload) {
+      try {
+              // const isRead=payload.isRead
+              // console.log(isRead)
+          const data = await this.updateOneEntity({'_id':payload.Id},{'isRead':true});
+          return data;
+      } catch (error) {
+          utils.consolelog('error', error, true);
+          return Promise.reject(error);
+      }
+  }
+  async countNotification() {
+    try {
+        const count = await this.count({'isRead':false});
+        return count;
+    } catch (error) {
+        utils.consolelog('error', error, true);
+        return Promise.reject(error);
+    }
+}
+
 }
 
 export const NotificationE = new NotificationEntities();
