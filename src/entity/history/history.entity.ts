8e5d43ce86ccd3dfcf9f0152cb7cb329b20ab85c
shivam.singh1_devs <shivam.singh1@appinventiv.com>
2022-03-10 18:34:49 +0530

    audit trail

--- src/entity/history/history.entity.ts
+++ src/entity/history/history.entity.ts
@@ -53,18 +53,21 @@ class HistoryEntities extends BaseEntity {
 //       return Promise.reject(error);
 //     }
 //   }
-
+    
   async getDifference(prevData, newData) {
+    let allID:String[]=["assignedTo","_id","adminId","userId","bankId"];
         function changes(newData, prevData) {
             let arrayIndexCounter = 0
 
             return lodash.transform(newData, function (result, value, key) {
-             if(key!=="_id"){
+             if(allID.indexOf(key)===-1){
               if (lodash.isEqual(value, prevData[key])) {
                 let resultKey = lodash.isArray(prevData) ? arrayIndexCounter++ : key
+                if(allID.indexOf(resultKey)===-1){
                 result[resultKey] = (lodash.isObject(value) && lodash.isObject(prevData[key])) ? changes(value, prevData[key]) : value
                 console.log(result)
                 console.log(result[resultKey])
+                }
               }
             }
               // if(!Object.values(result).length) delete result
